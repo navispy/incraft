03c0d1731958e94f188b86a0c26d8181d6de37de
navispy <valery@navispy.com>
2021-04-25 00:05:00 +0300

    Main page design 2

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <title>INCRAFTBY</title>
 
     <link rel="stylesheet" href="css/styles.css?version=1" type="text/css">
+    <link rel="stylesheet" href="css/menu.css?version=1" type="text/css">
     <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
 
     <script src="js/jquery.min.js" type='text/javascript'></script>
@@ -23,25 +24,22 @@
 
     <div class="wrapper">
         <div class="header">
-            <a href="index.html" class="farmstead-logo-img">
+            <a href="index.html" class="logo">
                 <img src="img/logo.svg" alt="ІnagroBy" />
             </a>
-            <!--</div>-->
 
-            <div clas="login">
+            <div class="login">
                 <h4 onclick="login()" id="inagro-user" class="sign-in__text" style="cursor:pointer; float:right;">ВОЙТИ</h4>
-                <img onclick="login()" id="login-icon-login" src="../../assets/icons/header/lock.svg" alt="lock" class="sign-in__img" style="cursor:pointer; float:right;padding-left:4px;">
-                <img onclick="logout()" id="login-icon-logout" src="../../assets/icons/header/exit.svg" alt="выйти" class="sign-in__img" style="visibility:hidden; cursor:pointer; float:right;padding-left:4px;">
             </div>
         </div>
         <div class="menu">
             <!-- Main Top Menu -->
             <nav>
                 <div id="navbar">
-                    <div id="logo" class="reverse">
+                    <div id="navbar-logo" class="reverse">
                         <div class="mobile-btn" style="font-size:30px;cursor:pointer; font-weight:bold;" onclick="openNav()">&#9776;</div>
                     </div>
-                    <div id="links">
+                    <div id="navbar-links">
                         <a href="index.html">Агроусадьбы</a>
                         <a href="services-additional.php">Допуслуги</a>
                         <a href="about.php">О нас</a>
@@ -51,20 +49,7 @@
                         <a onclick="checkProfile()">Личный кабинет</a>
                     </div>
                 </div>
-
             </nav>
-            <!-- Mobile Menu -->
-            <div id="mySidenav" class="sidenav">
-                <a style="cursor:pointer;" class="closebtn" onclick="closeNav()">&times;</a>
-                <a href="index.htm;">Агроусадьбы</a>
-                <a href="services-additional.php">Допуслуги</a>
-                <a href="about.php">О нас</a>
-                <!--<a href="">Владельцам агроусадеб</a>-->
-                <a href="../blog/">Статьи</a>
-                <!--<a href="profile.php">Личный кабинет</a>-->
-                <a onclick="checkProfile()">Личный кабинет</a>
-            </div>
-
         </div>
     </div>
 </body>
