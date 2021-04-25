2949cfad678a95893d86834ea5925dce7254b60a
navispy <valery@navispy.com>
2021-04-25 18:03:47 +0300

    Main page design 3

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+?>
+
+<footer class="grid-item footer">
+    <div class="grid-item footer__contacts">
+        <h4>Контакты:</h4>
+        <div class="footer__contacts-wrap">
+            <a href="tel:2895-45-15" class="tel">2895-45-15</a>
+            <a href="mailto:byagro@gmail.com" class="mail">byagro@gmail.com</a>
+            <p>г. Быхов, ул. Туристов 15</p>
+        </div>
+    </div>
+    <div class="grid-item footer__text-wrap">
+        <p class="footer__text">Сайт разработан в рамках реализации инициативы «Быхов-инновационный», в рамках проекта «Поддержка экономического развития на местном уровне в Республике Беларусь», который финансируется Европейским союзом (ЕС) и реализуется Программой развития ООН (ПРООН) в партнерстве с Министерством экономики РБ.</p>
+        <br>
+        <p class="footer__text">Изложенная информация не отражает официальную позицию Европейского союза, ПРООН и Министерства экономики Республики Беларусь.</p>
+    </div>
+    <div class="grid-item footer__partners partners">
+        <div class="partner"><img src="img/EU.png" alt="EU" class="partner__image"></div>
+        <div class="partner"> <img src="img/ministry of economy.png" alt="ministry of economy" class="partner__image"></div>
+        <div class="partner"><img src="img/UNDP.png" alt="UNDP" class="partner__image"></div>
+    </div>
+
+</footer>
--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>INCRAFTBY</title>
 
+    <link rel="stylesheet" href="css/fonts.css" type="text/css">
     <link rel="stylesheet" href="css/styles.css?version=1" type="text/css">
     <link rel="stylesheet" href="css/menu.css?version=1" type="text/css">
     <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
@@ -24,12 +25,37 @@
 
     <div class="wrapper">
         <div class="header">
-            <a href="index.html" class="logo">
+            <a href="index.php" class="logo">
                 <img src="img/logo.svg" alt="ІnagroBy" />
             </a>
 
             <div class="login">
-                <h4 onclick="login()" id="inagro-user" class="sign-in__text" style="cursor:pointer; float:right;">ВОЙТИ</h4>
+                <div class="login-commands">
+                    <img class="login-commands-user" src="img/user01.png">
+                    <span>Выход</h4>
+                </div>
+
+                <div class="login-notifications">
+                    <svg class="login-notifications-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20.4 17.5H5L6 8.1C6.3 5.3 9.2 3.2 12.7 3.2C16.2 3.2 19.2 5.4 19.4 8.1L20.4 17.5Z" fill="white" stroke="black" stroke-width="2" stroke-miterlimit="10" />
+                        <path d="M14.9999 20.3C14.9999 21.4 13.9999 22.2 12.6999 22.2C11.3999 22.2 10.3999 21.3 10.3999 20.3" fill="white" />
+                        <path d="M14.9999 20.3C14.9999 21.4 13.9999 22.2 12.6999 22.2C11.3999 22.2 10.3999 21.3 10.3999 20.3" stroke="black" stroke-width="2" stroke-miterlimit="10" />
+                        <path d="M12.7 2V3.2" stroke="black" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                    </svg>
+                    <div class="login-notifications-counter">
+                        <span class="login-notifications-counter-span">43</span>
+                    </div>
+                </div>
+
+                <div class="login-shop">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M22 9H2L5 4H19L22 9Z" stroke="black" stroke-width="2" stroke-miterlimit="10" />
+                        <path d="M4 22V9H20V22" stroke="black" stroke-width="2" stroke-miterlimit="10" />
+                        <path d="M13 13H8V21H13V13Z" stroke="black" stroke-width="2" stroke-miterlimit="10" />
+                    </svg>
+
+                    <span>Магазин</span>
+                </div>
             </div>
         </div>
         <div class="menu">
@@ -37,20 +63,21 @@
             <nav>
                 <div id="navbar">
                     <div id="navbar-logo" class="reverse">
-                        <div class="mobile-btn" style="font-size:30px;cursor:pointer; font-weight:bold;" onclick="openNav()">&#9776;</div>
+                        <div class="mobile-btn" onclick="openNav()">&#9776;</div>
                     </div>
                     <div id="navbar-links">
-                        <a href="index.html">Агроусадьбы</a>
+                        <a href="index.php">Катало</a>
                         <a href="services-additional.php">Допуслуги</a>
                         <a href="about.php">О нас</a>
-                        <!--<a href="">Владельцам агроусадеб</a>-->
-                        <a href="../blog/">Статьи</a>
-                        <!--<a href="profile.php">Личный кабинет</a>-->
+                        <a href="../blog/">Ремесленникам</a>
+                        <a onclick="checkProfile()">Статьи</a>
                         <a onclick="checkProfile()">Личный кабинет</a>
                     </div>
                 </div>
             </nav>
         </div>
+
+        <?php include 'footer.php'; ?>
     </div>
 </body>
 
