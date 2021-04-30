29883fe38ad8e88de2c6e5ab006d86c02a6354ef
navispy <valery@navispy.com>
2021-05-01 02:46:09 +0300

    Main page design 6

--- main-content.php
+++ main-content.php
@@ -44,7 +44,256 @@
                     </div>
                 </div>
             </div>
+
+            <div class="cmd-img-left">
+                <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.90002 1.9L1.80002 9C1.30002 9.5 1.30002 10.4 1.80002 11L8.90002 18.1" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                    <path d="M3.20003 10H20.4" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                </svg>
+            </div>
+
+            <div class="cmd-img-right">
+                <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.1001 1.9L20.2001 9C20.7001 9.5 20.7001 10.4 20.2001 11L13.1001 18.1" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                    <path d="M18.8001 10H1.6001" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                </svg>
+            </div>
         </div>
     </div>
 
+    <div class="last-reviews">
+        <div class="text">
+            <span class="text-1">Последние <a class="span-link" href="#">отзывы</a></span>
+            <span class="text-2">Соломенная кукла “Дабрабыт”</span>
+            <span class="text-3">Отличная кукла! Это самая слолменная кукла из всех соломенных кукол!<br>У нее ручки из соломки, ножки из соломки, позико тоже из соломки!<br>Не из соломки у нее только ценник. К сожалению.</span>
+
+            <svg width="149" height="8" viewBox="0 0 149 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M148.354 4.35355C148.549 4.15829 148.549 3.84171 148.354 3.64645L145.172 0.464466C144.976 0.269204 144.66 0.269204 144.464 0.464466C144.269 0.659728 144.269 0.976311 144.464 1.17157L147.293 4L144.464 6.82843C144.269 7.02369 144.269 7.34027 144.464 7.53553C144.66 7.7308 144.976 7.7308 145.172 7.53553L148.354 4.35355ZM0 4.5H148V3.5H0V4.5Z" fill="black" />
+            </svg>
+        </div>
+
+        <img src="img/temp04.png" />
+
+        <div class="cmd-img-left">
+            <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8.90002 1.9L1.80002 9C1.30002 9.5 1.30002 10.4 1.80002 11L8.90002 18.1" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                <path d="M3.20003 10H20.4" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+            </svg>
+        </div>
+
+        <div class="cmd-img-right">
+            <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.1001 1.9L20.2001 9C20.7001 9.5 20.7001 10.4 20.2001 11L13.1001 18.1" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                <path d="M18.8001 10H1.6001" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+            </svg>
+        </div>
+    </div>
+
+
+    <div class="top-10-craftsmen">
+        <span>Топ 10 ремесленников и магазинов</span>
+        <div class="container">
+            <div class="item">
+                <img src="img/temp05.png" />
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">ИП Гундарь</span>
+                        <span class="desc">Ковка, сварка, пилка</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/temp06.png" />
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">Магазин “Варажэя”</span>
+                        <span class="desc">Изделия из соломы, вышивка</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/temp07.png" />
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">Сергеевич Валентин</span>
+                        <span class="desc">Ремесленник. Мебель из дуба под заказ</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="cmd-img-left">
+                <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M8.90002 1.9L1.80002 9C1.30002 9.5 1.30002 10.4 1.80002 11L8.90002 18.1" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                    <path d="M3.20003 10H20.4" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                </svg>
+            </div>
+
+            <div class="cmd-img-right">
+                <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.1001 1.9L20.2001 9C20.7001 9.5 20.7001 10.4 20.2001 11L13.1001 18.1" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                    <path d="M18.8001 10H1.6001" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                </svg>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="holidays">
+        <span>Праздники INCRAFTBY</span>
+        <div class="container">
+            <img src="img/temp08.png" />
+            <div class="info">
+                <div class="details">
+                    <span class="info-1">Праздник ремесел в Быхове</span>
+                    <span class="info-2">24 фото</span>
+                    <span class="info-3">Если вы любите этот праздник и готовы пить три-четыре дня без остановки, а так же водить хоровод и спать голым в снегу, то рекомендуем приехать в Быхов. У нас тут будет весело </span>
+
+                    <svg width="107" height="8" viewBox="0 0 107 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M106.75 4.35355C106.946 4.15829 106.946 3.84171 106.75 3.64645L103.568 0.464466C103.373 0.269204 103.056 0.269204 102.861 0.464466C102.666 0.659728 102.666 0.976311 102.861 1.17157L105.69 4L102.861 6.82843C102.666 7.02369 102.666 7.34027 102.861 7.53553C103.056 7.7308 103.373 7.7308 103.568 7.53553L106.75 4.35355ZM0.753906 4.5H106.397V3.5H0.753906V4.5Z" fill="white" />
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="latest-works">
+        <span>Последние добавленные работы</span>
+        <div class="container">
+            <div class="item">
+                <div>
+                    <img src="img/temp01.png" />
+                </div>
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">Серьги "Папараць"</span>
+                        <span class="desc">Бронза, малахит, что-там еще и немного чего-то еще</span>
+                    </div>
+                    <div class="price">
+                        <span class="shop-name">ИП "Дел Мастер"</span>
+                        <span class="value">1500 руб</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div>
+                    <img src="img/temp09.png" />
+                </div>
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">Серьги "Папараць"</span>
+                        <span class="desc">Бронза, малахит, что-там еще и немного чего-то еще</span>
+                    </div>
+                    <div class="price">
+                        <span class="shop-name">ИП "Дел Мастер"</span>
+                        <span class="value">1500 руб</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div>
+                    <img src="img/temp10.png" />
+                </div>
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">Серьги "Папараць"</span>
+                        <span class="desc">Бронза, малахит, что-там еще и немного чего-то еще</span>
+                    </div>
+                    <div class="price">
+                        <span class="shop-name">ИП "Дел Мастер"</span>
+                        <span class="value">1500 руб</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div>
+                    <img src="img/temp11.png" />
+                </div>
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">Серьги "Папараць"</span>
+                        <span class="desc">Бронза, малахит, что-там еще и немного чего-то еще</span>
+                    </div>
+                    <div class="price">
+                        <span class="shop-name">ИП "Дел Мастер"</span>
+                        <span class="value">1500 руб</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div>
+                    <img src="img/temp12.png" />
+                </div>
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">Серьги "Папараць"</span>
+                        <span class="desc">Бронза, малахит, что-там еще и немного чего-то еще</span>
+                    </div>
+                    <div class="price">
+                        <span class="shop-name">ИП "Дел Мастер"</span>
+                        <span class="value">1500 руб</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div>
+                    <img src="img/temp13.png" />
+                </div>
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">Серьги "Папараць"</span>
+                        <span class="desc">Бронза, малахит, что-там еще и немного чего-то еще</span>
+                    </div>
+                    <div class="price">
+                        <span class="shop-name">ИП "Дел Мастер"</span>
+                        <span class="value">1500 руб</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div>
+                    <img src="img/temp14.png" />
+                </div>
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">Серьги "Папараць"</span>
+                        <span class="desc">Бронза, малахит, что-там еще и немного чего-то еще</span>
+                    </div>
+                    <div class="price">
+                        <span class="shop-name">ИП "Дел Мастер"</span>
+                        <span class="value">1500 руб</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div>
+                    <img src="img/temp15.png" />
+                </div>
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">Серьги "Папараць"</span>
+                        <span class="desc">Бронза, малахит, что-там еще и немного чего-то еще</span>
+                    </div>
+                    <div class="price">
+                        <span class="shop-name">ИП "Дел Мастер"</span>
+                        <span class="value">1500 руб</span>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div>
+                    <img src="img/temp16.png" />
+                </div>
+                <div class="info">
+                    <div class="name-desc">
+                        <span class="name">Серьги "Папараць"</span>
+                        <span class="desc">Бронза, малахит, что-там еще и немного чего-то еще</span>
+                    </div>
+                    <div class="price">
+                        <span class="shop-name">ИП "Дел Мастер"</span>
+                        <span class="value">1500 руб</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
