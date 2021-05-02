179303eef3db5fb16ee626296b09294bed6ca171
navispy <valery@navispy.com>
2021-05-03 01:58:27 +0300

    Item page design final

--- item-watched.php
+++ item-watched.php
@@ -1,5 +1,75 @@
 <?php
 ?>
-<div class="other">
+<?php
+?>
+<div class="watched">
     <span>Ранее Вы просматривали</span>
+    <div class="container">
+        <div class="item">
+            <img src="img/temp19.png" />
+            <div class="info">
+                <div class="name-desc">
+                    <span class="name">Серьги “вязь”</span>
+                    <span class="desc">Золото</span>
+                </div>
+                <div class="price">
+                    <span class="shop-name">ИП "Дел Мастер"</span>
+                    <span class="value">1500 руб</span>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="img/temp20.png" />
+            <div class="info">
+                <div class="name-desc">
+                    <span class="name">Серьги “Папараць”</span>
+                    <span class="desc">Бронза, малахит, что-то там еще и немного чего-то еще</span>
+                </div>
+                <div class="price">
+                    <span class="shop-name">ИП "Дел Мастер"</span>
+                    <span class="value">1500 руб</span>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="img/temp21.png" />
+            <div class="info">
+                <div class="name-desc">
+                    <span class="name">Серьги “Виноград”</span>
+                    <span class="desc">Самоцветы</span>
+                </div>
+                <div class="price">
+                    <span class="shop-name">ИП "Дел Мастер"</span>
+                    <span class="value">1500 руб</span>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="img/temp22.png" />
+            <div class="info">
+                <div class="name-desc">
+                    <span class="name">Изумруд</span>
+                    <span class="desc">Хрусталь</span>
+                </div>
+                <div class="price">
+                    <span class="shop-name">ИП "Дел Мастер"</span>
+                    <span class="value">1500 руб</span>
+                </div>
+            </div>
+        </div>        
+
+        <div class="cmd-img-left">
+            <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8.90002 1.9L1.80002 9C1.30002 9.5 1.30002 10.4 1.80002 11L8.90002 18.1" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                <path d="M3.20003 10H20.4" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+            </svg>
+        </div>
+
+        <div class="cmd-img-right">
+            <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.1001 1.9L20.2001 9C20.7001 9.5 20.7001 10.4 20.2001 11L13.1001 18.1" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+                <path d="M18.8001 10H1.6001" stroke="white" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" />
+            </svg>
+        </div>
+    </div>
 </div>
