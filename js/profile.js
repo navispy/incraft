085f4622ef1c0a9102342349f7c3bc2b66ebb338
navispy <45857115+navispy@users.noreply.github.com>
2021-11-27 22:57:15 +0300

    Profile - add the Shop creation button handler

--- js/profile.js
+++ js/profile.js
@@ -3,10 +3,10 @@ const ACCOUNT_SUSPENDED = 1;
 
 var profile;
 
-function updateCmdAccountStatus (status) {
+function updateCmdAccountStatus(status) {
     let accountCmdText = "";
     let statusNext; //as we toggle status
-    switch(status) {
+    switch (status) {
         case ACCOUNT_SUSPENDED:
             accountCmdText = "Возобновить действие аккаунта";
             statusNext = ACCOUNT_ACTIVE;
@@ -17,7 +17,7 @@ function updateCmdAccountStatus (status) {
             break;
         default:
             break;
-    }  
+    }
 
     $(".cmd-account-status.suspend-account span").html(accountCmdText);
     $(".cmd-account-status").data("value", statusNext);
@@ -26,25 +26,25 @@ function updateCmdAccountStatus (status) {
 }
 
 async function getUserProfile(userID) {
-    
+
     let response = await fetch(`get_json_user_profile.php?userID=${userID}`)
-    .catch(e => {
-        console.log('Error: ' + e.message);
-    });
+        .catch(e => {
+            console.log('Error: ' + e.message);
+        });
 
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     let profile = await response.json();
-    return profile; 
+    return profile;
 }
 
 async function showProfile(userID) {
     profile = await getUserProfile(userID)
-    .catch(e => {
-        console.log('There has been a problem with your fetch operation: ' + e.message);
-    });
+        .catch(e => {
+            console.log('There has been a problem with your fetch operation: ' + e.message);
+        });
 
     let name = profile["Name"];
     let dateRegisteredUnfixed = profile["DateRegistered"];
@@ -65,7 +65,7 @@ async function showProfile(userID) {
     $(".input-email").val(email);
     $(".input-phone").val(phone);
     $(`input[name='visibility-option'][value='${visibility}']`).prop('checked', true);
-    
+
     $(".rcv-msg-from-visitors").prop("checked", receiveMsgFromVisitors);
     $(".rcv-msg-from-system").prop("checked", receiveMsgFromSystem);
     $(".rcv-msg-from-favourite-shops").prop("checked", receiveMsgFromFavouriteShops);
@@ -117,7 +117,7 @@ async function saveProfile() {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
         },
-        body: Object.entries(params).map(([k,v])=>{return k+'='+v}).join('&')
+        body: Object.entries(params).map(([k, v]) => { return k + '=' + v }).join('&')
     });
 
     let data = await response.json();
@@ -142,51 +142,55 @@ function setupHandlers() {
         let control = $(this).data("control");
         $(`.${control}`).attr("readonly", false);
         $(`.${control}`).addClass("input-border");
-        $(`.${control}`).focus(); 
-     });
- 
-     $(".tabs-3 .data .input").on("input", function () {
-         let field = $(this).data("field");
-         let newValue = $(this).val();
-         profile[field] = newValue;
-     });
- 
-     $("input[name='visibility-option']").change(function(event) {
-         let field = $(this).data("field");
-         let newValue = $(this).val();
-         profile[field] = parseInt(newValue);
-     });
- 
-     $(".rcv-msg").change(function () {
-         let field = $(this).data("field");
-         let newValue = $(this).prop("checked");
-         profile[field] = newValue;
-     });
-  
-     $(".cmd-account-status").click(function () {
-         let field = $(this).data("field");
-         let newValue = $(this).data("value");
-         profile[field] = newValue;
- 
-         updateCmdAccountStatus(newValue);
-     });
- 
-     $(".button-edit").click(function () { // select an avatar
-         $(".new-avatar").click();
-     });
- 
-     $(".new-avatar").on("input", function () { //upload an avatar
-         updateAvatar(this);
-     });
- 
-     $(".save-button").click(function () {
-         saveProfile();
-     });
+        $(`.${control}`).focus();
+    });
 
-     $(".cmd-shop-create").click(function() {
+    $(".tabs-3 .data .input").on("input", function () {
+        let field = $(this).data("field");
+        let newValue = $(this).val();
+        profile[field] = newValue;
+    });
+
+    $("input[name='visibility-option']").change(function (event) {
+        let field = $(this).data("field");
+        let newValue = $(this).val();
+        profile[field] = parseInt(newValue);
+    });
+
+    $(".rcv-msg").change(function () {
+        let field = $(this).data("field");
+        let newValue = $(this).prop("checked");
+        profile[field] = newValue;
+    });
+
+    $(".cmd-account-status").click(function () {
+        let field = $(this).data("field");
+        let newValue = $(this).data("value");
+        profile[field] = newValue;
+
+        updateCmdAccountStatus(newValue);
+    });
+
+    $(".button-edit").click(function () { // select an avatar
+        $(".new-avatar").click();
+    });
+
+    $(".new-avatar").on("input", function () { //upload an avatar
+        updateAvatar(this);
+    });
+
+    $(".tabs-1-wrapper .save-button").click(function () {
+        alert("Create a shop!");
+    });
+
+    $(".tabs-3 .save-button").click(function () {
+        saveProfile();
+    });
+
+    $(".cmd-shop-create").click(function () {
         $(".tabs-1-wrapper .page-1").css("display", "none");
         $(".tabs-1-wrapper .page-2").css("display", "flex");
-     });
+    });
 }
 
 $(document).ready(function () {
